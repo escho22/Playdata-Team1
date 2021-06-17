a49b937399f44b1c9d739201122b735a4d917b3a
miscaminos <sojung1221@gmail.com>
2021-06-17 10:48:21 +0900

    add quiz jsp, js

--- SignLanEduService/src/main/java/com/example/SignLanEduService/controller/QuizController.java
+++ SignLanEduService/src/main/java/com/example/SignLanEduService/controller/QuizController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,7 +19,10 @@ import com.example.SignLanEduService.service.MemberService;
 import com.example.SignLanEduService.service.QuizService;
 import com.example.SignLanEduService.service.WordService;
 import com.example.SignLanEduService.vo.QuizVO;
+import com.example.SignLanEduService.vo.WordVO;
 
+
+@CrossOrigin(origins = "http://localhost:8000") 
 @Controller
 public class QuizController {
 
@@ -39,17 +43,25 @@ public class QuizController {
 		System.out.println("--> QuizController created.");
 	}
 	
+	
 	@RequestMapping(value = "/quiz/select", method = RequestMethod.GET)
-	public ModelAndView selectQuiz() {
+	public ModelAndView selectQuiz(HttpSession session) {
 		ModelAndView mav = new ModelAndView();
-		mav.setViewName("/quiz/select");
+		// 로그인여부 확인
+		if (!mservice.isMember(session)) {
+			mav.setViewName("redirect:/member/login_ck_form"); // login 페이지로 이동
+		} else {
+			mav.setViewName("/quiz/select");
+		}
 		return mav;
 	}
 	
 	@RequestMapping(value = "/quiz/level1/choose", method = RequestMethod.GET)
 	public ModelAndView quizLevel1() {
+		
 		ModelAndView mav = new ModelAndView();
 		mav.setViewName("/quiz/level1/choose");
+
 		return mav;
 	}
 	
@@ -70,6 +82,8 @@ public class QuizController {
 	@RequestMapping(value = "/quiz/level4/choose", method = RequestMethod.GET)
 	public ModelAndView quizLevel4() {
 		ModelAndView mav = new ModelAndView();
+		String[] words = wservice.get_words(1); 
+		mav.addObject("words", words);
 		mav.setViewName("/quiz/level4/choose");
 		return mav;
 	}
@@ -77,6 +91,9 @@ public class QuizController {
 	@RequestMapping(value = "/quiz/level4/quiz_4_1", method = RequestMethod.GET)
 	public ModelAndView quizLevel4_1() {
 		ModelAndView mav = new ModelAndView();
+		WordVO word = wservice.get_word(1); 
+		mav.addObject("word", word.getW_word());
+		mav.addObject("index", word.getW_index());
 		mav.setViewName("/quiz/level4/quiz_4_1");
 		return mav;
 	}
@@ -88,51 +105,10 @@ public class QuizController {
 		return mav;
 	}
 
-	//quiz intro page로 이동 - 전체 levels 보여주기
-	@RequestMapping(value = "/quiz/intro", method = RequestMethod.GET)
-	public ModelAndView intro(HttpSession session) {
-		
-		ModelAndView mav = new ModelAndView();
-		//로그인여부 확인
-		if(!mservice.isMember(session)) {
-		    mav.setViewName("redirect:/member/login_ck_form"); //login 페이지로 이동
-		}
-		else {
-			String[] all_levels = wservice.list_levels();
-			mav.addObject("all_levels", all_levels); //all_levels를 intro페이지에 출력필요
-			mav.setViewName("/quiz/intro");
-		}
-		return mav;
-	}
-	
-	//선택된 level의 quiz page로 이동 (views/quiz/main.jsp에서 js로 webcam구동+ajax방식으로 word하나씩 출력해야함)
-//	@RequestMapping(value = "/quiz/start", method = RequestMethod.GET)
-//	public ModelAndView start(int m_num, int level) {//sessionScope.m_num으로 m_num 전달받으면됨.
-//		
-//		ModelAndView mav = new ModelAndView();
-//
-//		MemberVO vo = mservice.read(m_num);
-//		String[] words = wservice.get_words(level); //level에 해당하는 단어들 반환
-//		
-//		mav.addObject("level", level);
-//		mav.addObject("words", words);
-//		mav.addObject("member", vo);
-//		mav.setViewName("/quiz/start");
-//
-//		return mav;
-//	}
-	
-	@RequestMapping(value = "/quiz/start", method = RequestMethod.GET)
-	public ModelAndView start() {//sessionScope.m_num으로 m_num 전달받으면됨.
-		
-		ModelAndView mav = new ModelAndView();
-		mav.setViewName("/quiz/start");
-		return mav;
-	}
 	
 	//quiz단어별 푼 결과 기록생성 - views/quiz/main.jsp로 어떻게 다시 돌아갈지???
 	@ResponseBody
-	@RequestMapping(value = "/quiz/start/{w_num}/result", method = RequestMethod.GET)
+	@RequestMapping(value = "/quiz/level4/quiz_4_1/result", method = RequestMethod.GET)
 	public ResponseEntity<String> create(
 			QuizVO vo, int m_num, int correct_cnt, @PathVariable("w_num") int w_num) {
 		
@@ -151,22 +127,13 @@ public class QuizController {
 		
 		ModelAndView mav = new ModelAndView();
 		mav.addObject("level", level);
+		//make a problem counter as an attribute in session?
 		mav.addObject("correct_cnt", correct_cnt);
 		mav.setViewName("/quiz/finish");
 
 		return mav;
 	}
-	
-//	//quiz word 가져오려했으나, view/quiz/main.jsp에서 ajax방식을 사용하는것이 더 나을것 같아서 주석처리
-//	@ResponseBody
-//	@GetMapping("/quiz/start/{w_num}")
-//	public ResponseEntity<WordVO> get_word(@PathVariable("w_num") int w_num){
-//		WordVO word = wservice.get_word(w_num);
-//		???
-//		return flag==1?new ResponseEntity<String>("success",HttpStatus.OK)
-//				:new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-//		
-//	}
+
 	
 		
 }
--- SignLanEduService/src/main/resources/static/js/webcam_detection.js
+++ SignLanEduService/src/main/resources/static/js/webcam_detection.js
@@ -1,103 +1,71 @@
-/**
- * 
- */
-const video = document.getElementById('webcam');
-const liveView = document.getElementById('liveView');
-const demosSection = document.getElementById('demos');
-const enableWebcamButton = document.getElementById('webcamButton');
+var classes;
+var video;
+var capture;
 
-// Check if webcam access is supported.
-function getUserMediaSupported() {
-  return !!(navigator.mediaDevices &&
-    navigator.mediaDevices.getUserMedia);
+function getFrame(video){
+	capture = video.get();
+	//draw capture_show only on canvas
+	video.hide()
+	createCanvas(1280, 720)
+	image(capture, 0, 0, 1280, 720);
+	return capture
 }
 
-// If webcam supported, add event listener to button for when user
-// wants to activate it to call enableCam function which we will 
-// define in the next step.
-if (getUserMediaSupported()) {
-  enableWebcamButton.addEventListener('click', enableCam);
-} else {
-  console.warn('getUserMedia() is not supported by your browser');
-}
-
-// Enable the live webcam view and start classification.
-function enableCam(event) {
-  // Only continue if the COCO-SSD has finished loading.
-  if (!model) {
-    return;
-  }
-  
-  // Hide the button once clicked.
-  event.target.classList.add('removed');  
-  
-  // getUsermedia parameters to force video but not audio.
-  const constraints = {
-    video: true
-  };
-
-  // Activate the webcam stream.
-  navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
-    video.srcObject = stream;
-    video.addEventListener('loadeddata', predictWebcam);
-  });
-}
-
-// Pretend model has loaded so we can try out the webcam code.
-var model = true;
-demosSection.classList.remove('invisible');
-
-// Before we can use COCO-SSD class we must wait for it to finish
-// loading. Machine Learning models can be large and take a moment 
-// to get everything needed to run.
-// Note: cocoSsd is an external object loaded from our index.html
-// script tag import so ignore any warning in Glitch.
-cocoSsd.load().then(function (loadedModel) {
-  model = loadedModel;
-  // Show demo section now model is ready to use.
-  demosSection.classList.remove('invisible');
-});
-
-
-var children = [];
-
-function predictWebcam() {
-  // Now let's start classifying a frame in the stream.
-  model.detect(video).then(function (predictions) {
-    // Remove any highlighting we did previous frame.
-    for (let i = 0; i < children.length; i++) {
-      liveView.removeChild(children[i]);
-    }
-    children.splice(0);
+function setup() {
     
-    // Now lets loop through predictions and draw them to the live view if
-    // they have a high confidence score.
-    for (let n = 0; n < predictions.length; n++) {
-      // If we are over 66% sure we are sure we classified it right, draw it!
-      if (predictions[n].score > 0.66) {
-        const p = document.createElement('p');
-        p.innerText = predictions[n].class  + ' - with ' 
-            + Math.round(parseFloat(predictions[n].score) * 100) 
-            + '% confidence.';
-        p.style = 'margin-left: ' + predictions[n].bbox[0] + 'px; margin-top: '
-            + (predictions[n].bbox[1] - 10) + 'px; width: ' 
-            + (predictions[n].bbox[2] - 10) + 'px; top: 0; left: 0;';
+    //background(51);
+    video = createCapture(VIDEO);
+    video.size(1280, 720);
+	//video.hide();
 
-        const highlighter = document.createElement('div');
-        highlighter.setAttribute('class', 'highlighter');
-        highlighter.style = 'left: ' + predictions[n].bbox[0] + 'px; top: '
-            + predictions[n].bbox[1] + 'px; width: ' 
-            + predictions[n].bbox[2] + 'px; height: '
-            + predictions[n].bbox[3] + 'px;';
+    const button1 = document.getElementById('capture');
+    button1.addEventListener('click', async event => {
+      setTimeout(function(){capture = getFrame(video)},5000)
+	});
 
-        liveView.appendChild(highlighter);
-        liveView.appendChild(p);
-        children.push(highlighter);
-        children.push(p);
-      }
-    }
-    
-    // Call this function again to keep predicting when the browser is ready.
-    window.requestAnimationFrame(predictWebcam);
-  });
+	const button2 = document.getElementById('submit');
+	button2.addEventListener('click', async event =>{
+		
+      //load video pixels onto the canvas
+      capture.loadPixels();
+      //toDataURL(): convert video canvas to base64
+      const image_serialized = capture.canvas.toDataURL();
+
+      classes = "['12','15','21','29','37','45','46']";
+      const data = { classes, image_serialized };
+
+      //added to deal with CORS policy block issues
+      let headers = new Headers();
+      headers.append('Content-Type', 'application/json');
+      headers.append('Accept', 'application/json');
+      headers.append('Origin','http://localhost:8080');
+      //headers.append('Access-Control-Allow-Origin','http://localhost:8000');
+
+      const options = {
+        mode: 'cors',
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify(data)
+      };
+      const response = await fetch('http://localhost:8000/result', options);
+      const json = await response.json();
+      console.log(json);
+
+	  //draw bounding box and write result with data from JSON
+	  const json_object = JSON.parse(json);
+	  const word_idx = json_object.word_index;
+	  const box = json_object.box_coord;
+	  const conf = json_object.confidence;
+	  const ratio = 540/540;
+	  
+	  textSize(32);
+	  text(word_idx, 10, 30);
+	  text(conf, 10, 60);
+	  fill(0, 102, 153, 51);
+	  rect((box[0]*ratio),(box[1]*ratio),(box[2]*ratio),(box[3]*ratio));
+
+	  
+	});  
 }
+
+
