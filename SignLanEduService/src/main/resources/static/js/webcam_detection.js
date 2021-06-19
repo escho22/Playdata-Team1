9d8006dec2fe960c0dc4a6cd90dc205b0f71cc40
miscaminos <sojung1221@gmail.com>
2021-06-19 16:01:56 +0900

    updated quiz module's MVC controller parts for DB saving and reorganized contents in quiz page

--- SignLanEduService/src/main/resources/static/js/webcam_detection.js
+++ SignLanEduService/src/main/resources/static/js/webcam_detection.js
@@ -1,30 +1,36 @@
-var classes;
-var video;
+
+var webcam;
 var capture;
+var mycanvas;
+
+var classes;
 var word_idx = 0;
 var conf = 0;
-var bar;
- 
-function getFrame(video){
-	capture = video.get();
+var answer_key = document.getElementById("answer").innerHTML; 
+var result = 0;
+
+
+function getFrame(webcam){
+	capture = webcam.get();
 	//draw capture_show only on canvas
-	video.hide()
-	createCanvas(1280, 720)
-	image(capture, 0, 0, 1280, 720);
-	run_progressbar(bar);
+	webcam.hide();
+	
+	mycanvas = createCanvas(1280, 720);
+	mycanvas.parent("container-canvas2");
+	
+	image(capture, 155, 0, 1280, 720);
 	return capture
 }
 
 function setup() {
-
-    video = createCapture(VIDEO);
-    video.size(1280, 720);
-
-	//draw_progressbar(bar);
+	//VIDEO= document.createElement("VIDEO");
+    webcam = createCapture(VIDEO);
+    webcam.size(1280, 720);
+	webcam.parent("container-canvas1");
 	
     const button1 = document.getElementById('capture');
     button1.addEventListener('click', async event => {
-      setTimeout(function(){capture = getFrame(video)},5000)
+      setTimeout(function(){capture = getFrame(webcam)},5000)
 	});
 
 	const button2 = document.getElementById('submit');
@@ -62,22 +68,32 @@ function setup() {
 	  const ratio = 540/540;
 
 	  //draw bounding box and write result with data from JSON
-	  textSize(32);
-	  text(word_idx, 10, 30);
-	  text(conf, 10, 60);
+	  if(word_idx != 'INCORRECT' && word_idx === answer_key){
+		  textSize(32);
+		  text(word_idx, 10, 30);
+		  text(conf, 10, 60);
+		  result = 1;	
+	  }
+	  else {
+		  textSize(32);
+		  text("INCORRECT", 10, 30);
+	  }
 	  fill(0, 102, 153, 51);
-	  rect((box[0]*ratio),(box[1]*ratio),(box[2]*ratio),(box[3]*ratio));
+	  rect((box[0]*ratio+155),(box[1]*ratio),(box[2]*ratio),(box[3]*ratio));
 	  
 	  check();
-	}); 	
-	 
+
+	  //pass values to jsp html
+	  document.getElementById("q_correct").value = result;
+	  document.getElementById("q_percent").value = conf;
+
+	}); 	 
 }
 
 function check(){
-	var answer_key = document.getElementById("answer").innerHTML;
-	console.log("answer_key: ",answer_key);
+	//console.log("answer_key: ",answer_key);
 	word_idx = word_idx.toString();
-	console.log("word_idx: ", word_idx);
+	//console.log("word_idx: ", word_idx);
 	
 	if (word_idx === answer_key){
 		document.getElementById("message").innerHTML="정답입니다!";
@@ -87,48 +103,6 @@ function check(){
 		document.getElementById("message").innerHTML="다시 기억해보세요";
 		document.getElementById("circle").style.visibility = 'hidden';
 	}
-
 }
 
-//function draw_progressbar(bar){
-//	
-//	var bar = new ProgressBar.SemiCircle(container, {
-//	  strokeWidth: 6,
-//	  color: '#FFEA82',
-//	  trailColor: '#eee',
-//	  trailWidth: 1,
-//	  easing: 'easeInOut',
-//	  duration: 1400,
-//	  svgStyle: null,
-//	  text: {
-//	    value: '',
-//	    alignToBottom: false
-//	  },
-//	  from: {color: '#FFEA82'},
-//	  to: {color: '#ED6A5A'},
-//	  // Set default step function for all animate calls
-//	  step: (state, bar) => {
-//	    bar.path.setAttribute('stroke', state.color);
-//	    var value = Math.round(bar.value() * 100);
-//	    if (value === 0) {
-//	      bar.setText('');
-//	    } else {
-//	      bar.setText(value);
-//	    }
-//
-//	    bar.text.style.color = state.color;
-//	  }
-//	});
-//	bar.text.style.fontFamily = '"Raleway", Helvetica, sans-serif';
-//	bar.text.style.fontSize = '2rem';
-//	
-//	document.getElementById("progressbar") = bar;
-//	document.getElementById("progressbar").style.visibility = 'hidden';
-//	
-//}
-
-function run_progressbar(bar){
-	document.getElementById("progressbar").style.visibility = 'visible';
-	bar.animate(1.0);
-}
 
